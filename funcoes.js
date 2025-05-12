358fa9613f3b595abf79a701e2ad3c947608ee85
Your Name"Lucas <lucas@outlook.com>
2025-05-11 21:36:31 -0300

    Adicionei botão de Mensagem e Terminei de comentar os códigos HTML e CSS

--- funcoes.js
+++ funcoes.js
@@ -1,9 +1,6 @@
-// funcoes.js
-function showMessage() {
-    document.getElementById('message').innerText = "Seja bem-vindo à minha página! Mantenha-se positivo e continue aprendendo!";
-}
 
-// Adicionando o evento de clique ao botão
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('botao').addEventListener('click', showMessage);
-});
+//Essa função está ligada com a ação de clicar com o botão e apresentar a mensagem
+function mostrarMensagem() {
+      const mensagem = "Meu trabalho de FDS merece nota 100 !!!!!";
+      document.getElementById("mensagem").innerText = mensagem;
+    }
